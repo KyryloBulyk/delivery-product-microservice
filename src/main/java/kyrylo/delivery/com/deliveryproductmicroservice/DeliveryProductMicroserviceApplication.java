9b5b8b9bced945d2d88c41d4a089bd806e221033
Kyrylo Bulyk <kyrylo.bulyk@gmail.com>
2024-03-30 18:09:02 +0100

    Initialize Delivery Product Microservice

---
+++ src/main/java/kyrylo/delivery/com/deliveryproductmicroservice/DeliveryProductMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package kyrylo.delivery.com.deliveryproductmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryProductMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryProductMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kyrylo/delivery/com/deliveryproductmicroservice/DeliveryProductMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package kyrylo.delivery.com.deliveryproductmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryProductMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
