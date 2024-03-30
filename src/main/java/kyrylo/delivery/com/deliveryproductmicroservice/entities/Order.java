93e4dbd66c9b245fc16defcca1f35ada9706f406
Kyrylo Bulyk <kyrylo.bulyk@gmail.com>
2024-03-30 19:30:27 +0100

    Created Entity Order

---
+++ src/main/java/kyrylo/delivery/com/deliveryproductmicroservice/entities/Order.java
@@ -0,0 +1,39 @@
+package kyrylo.delivery.com.deliveryproductmicroservice.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "order_products",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private Set<Product> products = new HashSet<>();
+
+    @Column(nullable = false)
+    private LocalDateTime orderDate;
+
+    @Column(nullable = false)
+    private double totalCost;
+}
