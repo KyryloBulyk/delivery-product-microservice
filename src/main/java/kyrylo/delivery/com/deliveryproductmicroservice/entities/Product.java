ba71fdcb6f0e9d2dc1b926ccd4a91b2dad138abe
Kyrylo Bulyk <kyrylo.bulyk@gmail.com>
2024-04-05 19:53:52 +0200

    Added validation to Entities

--- src/main/java/kyrylo/delivery/com/deliveryproductmicroservice/entities/Product.java
+++ src/main/java/kyrylo/delivery/com/deliveryproductmicroservice/entities/Product.java
@@ -1,5 +1,9 @@
 package kyrylo.delivery.com.deliveryproductmicroservice.entities;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,9 +18,14 @@ public class Product {
     @Id
     private String id;
 
+    @NotBlank(message = "Product name cannot be blank")
+    @Size(min = 1, max = 100, message = "Product name must be between 1 and 100 characters")
     private String name;
 
+    @NotNull(message = "Price cannot be null")
+    @Min(value = 0, message = "Price must be greater than or equal to 0")
     private double price;
 
+    @NotNull(message = "Category cannot be null")
     private Category category;
 }
